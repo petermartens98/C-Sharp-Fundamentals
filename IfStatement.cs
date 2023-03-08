1490f3c42362576278c58d44ff7ea33813873821
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-03-08 14:56:55 -0500

    Update IfStatement.cs

--- IfStatement.cs
+++ IfStatement.cs
@@ -6,6 +6,26 @@ namespace IfStatements
     {
         static void Main(string[] args)
         {
+            //if statement = a basic form of decision making
+            // -------------------- Example 1 -------------------- 
+
+            Console.WriteLine("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("You are now signed up!");
+            }
+            else if (age < 0)
+            {
+                Console.WriteLine("You haven't been born yet!");
+            }
+            else
+            {
+                Console.WriteLine("You must be 18+ to sign up!");
+            }            
+            // -------------------- Example 2 -------------------- 
+
             Console.WriteLine("Please enter your name: ");
             String name = Console.ReadLine();
 
